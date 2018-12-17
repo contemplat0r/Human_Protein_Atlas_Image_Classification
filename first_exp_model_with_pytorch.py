50bda8adfb7bff7dd8a16c05b8801984ca39b974
Contemplat0r <uldo.forme@gmail.com>
2018-12-17 07:03:27 +0200

    Experiments with Pythorch datasets and models

--- first_exp_model_with_pytorch.py
+++ first_exp_model_with_pytorch.py
@@ -5,48 +5,66 @@ import pandas as pd
 import numpy as np
 import matplotlib.image as mpimg
 from sklearn.preprocessing import MultiLabelBinarizer
+from PIL import Image
 
 import torch
 from torchvision import datasets, transforms, models
 from torch.utils import data
 
-classes = np.arange(0,28)
-multilabel_binarizer = MultiLabelBinarizer(classes)
-multilabel_binarizer.fit(classes)
+CLASSES = np.arange(0,28)
+multilabel_binarizer = MultiLabelBinarizer(CLASSES)
+multilabel_binarizer.fit(CLASSES)
 
 INPUT_DIR = '../input'
-
-def fill_targets(df_row, targets_column_name='Target'):
-    df_row[targets_column_name] = np.array(df_row[targets_column_name].split(' ')).astype(np.int8)
-    for label_name in LABEL_NAMES:
-        df_row[label_name] = 0
-    for num in df_row[targets_column_name]:
-        df_row[LABEL_NAMES[int(num)]] = 1
-    return df_row
+TRAIN_IMAGES_DIR = pathlib.Path(INPUT_DIR, 'train').as_posix()
+TEST_IMAGES_DIR = pathlib.Path(INPUT_DIR, 'test').as_posix()
+TARGETS_COLUMN_NAME = 'Target'
+COLORS = ('red', 'green', 'blue', 'yellow')
+IMAGE_FILE_EXT = 'png'
 
 class HumanProteinAtlasDataset(data.Dataset):
 
-    #def __init__(self, images_color_channels_fnames, transform=None):
-    def __init__(self, images_description_df, transform=None, train=True):
+    def __init__(self, images_description_df, transform=None, train_mode=True):
 
-        #self.images_color_channels_fnames = images_color_channels_fnames
         self.images_description_df = images_description_df.copy()
         self.transform = transform
-        self.train_mode = True
+        self.train_mode = train_mode
+        if train_mode:
+            self.path_to_img_dir = TRAIN_IMAGES_DIR
+        else:
+            self.path_to_img_dir = TEST_IMAGES_DIR
 
     def __len__(self):
-        return len(images_color_channels_fnames.keys())
+        return self.images_description_df.shape[0]
 
 
     def __getitem__(self, index):
-        return 
+        multilabel_target = None
+        color_image = self._load_multicolor_image(index)
+        if self.train_mode:
+            multilabel_target = self._load_multilabel_target(index)
+        if self.transform:
+                color_image = self.transform(color_image)
+        return color_image, multilabel_target
 
+    def _load_multicolor_image(self, index):
+        img_components_id = self.images_description_df.iloc[index]['Id']
+        print("_load_multicolor_image, img_components_id: ", img_components_id)
+        image_color_components = []
+        for color in COLORS:
+            path_to_color_component_file = pathlib.Path(
+                    self.path_to_img_dir, '{}_{}.{}'.format(
+                        img_components_id, color, IMAGE_FILE_EXT
+                    )
+                )
+            image_color_components.append(Image.open(path_to_color_component_file))
+        return Image.merge('RGBA', bands=image_color_components) 
 
-    def _load_imgs_color_channels(self):
-	self.imgs_color_chanels = {}
-	for img_grp_id, img_fnames in self.images_color_channels_fnames.items():
-	    grouped_color_channels = {
-		img_fname[:-4]: mpimg.imread(pathlib.Path(INPUT_DIR, img_fname).as_posix()) for img_fname in img_fnames
-	    }
-	    imgs_color_chanels[img_grp_id] = grouped_color_channels
-	return imgs_color_chanels
+    def _load_multilabel_target(self, index):
+        return multilabel_binarizer.transform(
+                [
+                    np.array(
+                        self.images_description_df[TARGETS_COLUMN_NAME].iloc[index].split(' ')
+                    ).astype(np.int8)
+                ]
+            )
--- load_data.py
+++ load_data.py
@@ -1,7 +1,29 @@
 import os
 import random
 
+import pathlib
+
+import numpy as np
 import pandas as pd
+
+import matplotlib.image as mpimg
+from sklearn.preprocessing import MultiLabelBinarizer
+from PIL import Image
+
+import torch
+from torchvision import datasets, transforms, models
+from torch.utils import data
+
+CLASSES = np.arange(0,28)
+multilabel_binarizer = MultiLabelBinarizer(CLASSES)
+multilabel_binarizer.fit(CLASSES)
+
+INPUT_DIR = '../input'
+TRAIN_IMAGES_DIR = pathlib.Path(INPUT_DIR, 'train').as_posix()
+TEST_IMAGES_DIR = pathlib.Path(INPUT_DIR, 'test').as_posix()
+TARGETS_COLUMN_NAME = 'Target'
+COLORS = ('red', 'green', 'blue', 'yellow')
+IMAGE_FILE_EXT = 'png'
 
 
 def show_data_dir_content(data_dir):
@@ -85,4 +107,51 @@ def load_imgs_color_groups(grouped_img_fnames):
         }
         imgs_color_groups[img_grp_id] = grouped_images
     return imgs_color_groups
+
+class HumanProteinAtlasDataset(data.Dataset):
+
+    def __init__(self, images_description_df, transform=None, train_mode=True):
+
+        self.images_description_df = images_description_df.copy()
+        self.transform = transform
+        self.train_mode = train_mode
+        if train_mode:
+            self.path_to_img_dir = TRAIN_IMAGES_DIR
+        else:
+            self.path_to_img_dir = TEST_IMAGES_DIR
+
+    def __len__(self):
+        return self.images_description_df.shape[0]
+
+
+    def __getitem__(self, index):
+        multilabel_target = None
+        color_image = self._load_multicolor_image(index)
+        if self.train_mode:
+            multilabel_target = self._load_multilabel_target(index)
+        if self.transform:
+                color_image = self.transform(color_image)
+        return color_image, multilabel_target
+
+    def _load_multicolor_image(self, index):
+        img_components_id = self.images_description_df.iloc[index]['Id']
+        #print("_load_multicolor_image, img_components_id: ", img_components_id)
+        image_color_components = []
+        for color in COLORS:
+            path_to_color_component_file = pathlib.Path(
+                    self.path_to_img_dir, '{}_{}.{}'.format(
+                        img_components_id, color, IMAGE_FILE_EXT
+                    )
+                )
+            image_color_components.append(Image.open(path_to_color_component_file))
+        return Image.merge('RGBA', bands=image_color_components) 
+
+    def _load_multilabel_target(self, index):
+        return multilabel_binarizer.transform(
+                [
+                    np.array(
+                        self.images_description_df[TARGETS_COLUMN_NAME].iloc[index].split(' ')
+                    ).astype(np.int8)
+                ]
+            )
 
