591e51131aed192fb0fb036db3dcb299c1559382
Contemplat0r <uldo.forme@gmail.com>
2018-12-28 20:30:39 +0200

    The validate() function added

--- load_data.py
+++ load_data.py
@@ -138,6 +138,27 @@ def prepare_loaders(dataset, valid_train_ratio=0.6):
     return loaders, dataset_sizes
 
 
+def validate(test_loader, model):
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        accuracy = correct / total
+        print(
+                "Test accuracy of the model on the 10000 test images: {} ".format(
+                    accuracy
+                )
+            )
+
+    return accuracy
+
 
 class HumanProteinAtlasDataset(data.Dataset):
 
